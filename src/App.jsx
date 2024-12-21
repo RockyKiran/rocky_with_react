e0a1959948059006208b5872df169e4e56e45472
RockyKiran <rockykiran8085@gmail.com>
2024-12-21 13:05:07 +0530

    1 First React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import RockyBotton from "./RockyInput";    //import means, when we use one component in another jsx file ,so we use import.
-import DynamicComp from "./DynamicComp";
-import RandomComp from "./RandomComp";
-function app() {
-  return <div>
-  
-      <h1>hey this side rocky is here</h1>
-      
-      <DynamicComp></DynamicComp>
-       <button>hate</button>
-       <RockyBotton></RockyBotton>
-       <RandomComp></RandomComp>
-       <RandomComp></RandomComp>
-       <RandomComp></RandomComp>
-       <RandomComp></RandomComp>
-       <RandomComp></RandomComp>
-  </div>        
-   
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-
-
-
-export default app;  //whatever component defines in inside the app() function ,when we want to use that omponent or function in any 
-                     //where,so we use "export default app;"".
+export default App

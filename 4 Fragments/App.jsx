a6d374e3f37afd480b2fa5a1fa6f6aa93c94a4c4
RockyKiran <rockykiran8085@gmail.com>
2024-12-21 13:18:41 +0530

    Learning Fragment

---
+++ 4 Fragments/App.jsx
@@ -0,0 +1,42 @@
+// import React from "react";
+
+function App() {
+ 
+
+  return ( 
+    // <React.Fragment>
+
+    // <h1>hello Fragments buddy</h1>
+    // <ul class="list-group">
+    //    <li class="list-group-item list-group-item-primary">A simple primary list group item</li>
+    //    <li class="list-group-item list-group-item-secondary">A simple secondary list group item</li>
+    //    <li class="list-group-item list-group-item-success">A simple success list group item</li>
+    //    <li class="list-group-item list-group-item-danger">A simple danger list group item</li>
+    //    <li class="list-group-item list-group-item-warning">A simple warning list group item</li>
+    //   <li class="list-group-item list-group-item-info">A simple info list group item</li>
+    //   <li class="list-group-item list-group-item-light">A simple light list group item</li>
+    //    <li class="list-group-item list-group-item-dark">A simple dark list group item</li>
+    // </ul>
+
+    // </React.Fragment>
+    
+    <>
+        <h1>Healthy Food</h1>
+        <ul class="list-group">
+        <li class="list-group-item list-group-item-primary">milk</li>
+        <li class="list-group-item list-group-item-secondary">Vegetables</li>
+        <li class="list-group-item list-group-item-success">Egg Masala</li>
+        <li class="list-group-item list-group-item-danger">Fruits</li>
+        <li class="list-group-item list-group-item-warning">Fish Fry</li>
+        <li class="list-group-item list-group-item-info">Mutton Handi</li>
+        <li class="list-group-item list-group-item-light">Chicken Curry</li>
+        <li class="list-group-item list-group-item-dark">Biriyani</li>
+        </ul>
+    
+    </>
+      
+    
+  )
+}
+
+export default App;

8f5189f4e253d1d1037b4a4d057c1289eb988439
RockyKiran <rockykiran8085@gmail.com>
2024-12-22 11:55:29 +0530

    How to set icon in React

---
+++ 11 react-icon/App.jsx
@@ -0,0 +1,19 @@
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { RiUninstallFill } from "react-icons/ri";
+
+function App() {
+
+  return (
+    <>
+      <h2>Welcome My Whatsapp <IoLogoWhatsapp/></h2> 
+      <h2>Welcome My Facebook <FaSquareFacebook /></h2>
+      <h2>Welcome My Instagram <FaInstagram /></h2>
+      <h2>Uninstall Bro <RiUninstallFill /></h2>
+
+    </>  
+  )
+}
+
+export default App
